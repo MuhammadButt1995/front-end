a39abea43fad228c67a1d3e06c41e8b1082fb6a5
Muhammad_Butt <Muhammadmbutt1995@gmail.com>
2023-04-22 19:05:16 -0400

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   const [internetStatus, setInternetStatus] = useState('Checking Internet...');
   const [domainStatus, setDomainStatus] = useState('Checking Domain...');
   const [azureStatus, setAzureStatus] = useState('Checking Azure...');
+  const [networkingData, setNetworkingData] = useState('Checking network...');
 
   // ... other useEffects (for Internet and Domain)
 
@@ -30,6 +31,29 @@ const Home = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8000/fminfo_networking/');
+
+    ws.onopen = () => {
+      console.log('WebSocket connection opened.');
+    };
+
+    ws.onmessage = event => {
+      console.log('Received message:', event.data);
+      const data = JSON.parse(event.data);
+      console.log('data after parser:', data);
+      setNetworkingData(JSON.stringify(data));
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket connection closed.');
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
   return (
     <>
       <h1>Internet Connection Status</h1>
@@ -38,6 +62,8 @@ const Home = () => {
       <p>{domainStatus}</p>
       <h1>Azure Connection Status</h1>
       <p>{azureStatus}</p>
+      <h1>Azure Connection Status</h1>
+      <p>{networkingData}</p>
     </>
   );
 };

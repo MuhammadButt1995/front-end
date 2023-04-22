eb67397a6a4e6e5f77167c52d808e8d28422b652
Muhammad_Butt <Muhammadmbutt1995@gmail.com>
2023-04-22 18:25:51 -0400

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-'use client';
 import React, { useEffect, useState } from 'react';
 
 const Home = () => {
   const [internetStatus, setInternetStatus] = useState('Checking Internet...');
   const [domainStatus, setDomainStatus] = useState('Checking Domain...');
+  const [azureStatus, setAzureStatus] = useState('Checking Azure...');
+
+  // ... other useEffects (for Internet and Domain)
 
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8000/internet_connection/');
+    const ws = new WebSocket('ws://localhost:8000/azure_connection/');
 
     ws.onopen = () => {
       console.log('WebSocket connection opened.');
@@ -15,8 +17,8 @@ const Home = () => {
     ws.onmessage = event => {
       console.log('Received message:', event.data);
       const data = JSON.parse(event.data);
-      console.log('data after paser:', data.connection.is_connected_to_internet);
-      setInternetStatus(JSON.stringify(data.connection.is_connected_to_internet));
+      console.log('data after paser:', data);
+      setAzureStatus(JSON.stringify(data));
     };
 
     ws.onclose = () => {
@@ -28,34 +30,14 @@ const Home = () => {
     };
   }, []);
 
-  useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8000/domain_connection/');
-
-    ws.onopen = () => {
-      console.log('WebSocket connection opened.');
-    };
-
-    ws.onmessage = event => {
-      console.log('Received message:', event.data);
-      const data = JSON.parse(event.data);
-      console.log('data after paser:', data.connection.connection_type);
-      setDomainStatus(JSON.stringify(data.connection.connection_type));
-    };
-
-    ws.onclose = () => {
-      console.log('WebSocket connection closed.');
-    };
-
-    return () => {
-      ws.close();
-    };
-  }, []);
   return (
     <>
       <h1>Internet Connection Status</h1>
       <p>{internetStatus}</p>
       <h1>Domain Connection Status</h1>
       <p>{domainStatus}</p>
+      <h1>Azure Connection Status</h1>
+      <p>{azureStatus}</p>
     </>
   );
 };
